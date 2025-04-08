e899547bac5b8cc764d44338682e4e5cf4a2874f
daehyeong2 <baconbacon1231@gmail.com>
2025-04-08 17:54:29 +0900

    Section 3: Types of Variable 1

---
+++ src/variable/Var7.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var7 {
+
+  public static void main(String[] args) {
+    int a = 100; // 정수 (100과 같은 개발자가 직접 입력한 값들을 리터럴이라 부름)
+    double b = 10.5; // 실수
+    boolean c = true; // 불리
+    char d = 'a'; // 문자
+    String e = "abc"; // 문자열
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+  }
+}
